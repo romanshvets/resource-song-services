0fc0c5aca68fb78a16162c9fb62e68092e45c587
Roman Shvets <roman.shvets.1989@gmail.com>
2026-06-03 11:47:19 +0300

    initial commit

---
+++ resource-service/src/main/java/com/romanshvets/resource/ResourceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.romanshvets.song;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceServiceApplication.class, args);
+	}
+
+}
